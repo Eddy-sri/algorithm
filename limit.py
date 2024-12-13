28e2af4f7cee221ad10f00e2b032a2a3a4b233eb
eddy <edrissoroori@gmail.com>
2024-12-13 17:23:51 +0330

    add doctest to limit file

--- limit.py
+++ limit.py
@@ -1,11 +1,13 @@
-"""
-arr ---> [1, 2, 3, 4, 5, 6, 7]
- min = 4 ---> [4, 5, 6, 7]
- max = 4 ---> [1, 2, 3, 4]
-
-"""
 
 def limit (arr, min = None, max = None):
+    """
+    >>> limit([1, 2, 3, 4, 5, 6, 7],4)
+    [4, 5, 6, 7]
+    >>> limit([1, 2, 3, 4, 5, 6, 7],None,4)
+    [1, 2, 3, 4]
+    >>> limit([1, 2, 3, 4, 5, 6, 7],2,5)
+    [2, 3, 4, 5]
+    """
     min_check = lambda val : True if min == None else (val >= min) 
     max_check = lambda val : True if max == None else (val <= max)
 
